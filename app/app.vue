aa58607bb5cba8d418ed7e9558cd512a2cdae4c2
Awen <awendml@gmail.com>
2026-05-05 09:55:18 +0800

    fix: netlify build

--- app/app.vue
+++ app/app.vue
@@ -63,7 +63,7 @@ const formatDate = (date: string) => {
   })
 }
 
-<!-- const fallbackImage = 'https://placehold.co/600x400/EEE/31343C' -->
+const fallbackImage = 'https://placehold.co/600x400/EEE/31343C'
 
 </script>
 
